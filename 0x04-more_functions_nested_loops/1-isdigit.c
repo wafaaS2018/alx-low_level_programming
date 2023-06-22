def3b2017e9755fd5cc398711de613982a021367
wafaaS2018 <wafaaomar141993@gmail.com>
2023-06-22 08:13:32 -0700

    Wafaa

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - Checks c if it is a digit
+ * @c: The character to check
+ *
+ * Return: 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+	if (c < 48 || c > 57)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
