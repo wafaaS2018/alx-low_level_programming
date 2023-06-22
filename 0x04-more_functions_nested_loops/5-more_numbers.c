281766e48892274338f22ea6c3596b6afa52ceb8
wafaaS2018 <wafaaomar141993@gmail.com>
2023-06-22 08:18:52 -0700

    Wafaa

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - Prints numbers 0 - 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			k = j / 10;
+			l = j % 10;
+			if (k != 0)
+			{
+				_putchar(k + 48);
+			}
+			_putchar(l + 48);
+		}
+		_putchar('\n');
+	}
+}
